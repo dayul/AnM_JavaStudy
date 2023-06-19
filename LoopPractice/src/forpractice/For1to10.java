635f123dcac5675e34b58f181ca0f22dddd1fb0d
dayul <dyulchoo@naver.com>
2023-06-19 20:56:53 +0900

    for문, 1부터 10까지 출력

---
+++ LoopPractice/src/forpractice/For1to10.java
@@ -0,0 +1,11 @@
+package forpractice;
+
+public class For1to10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=10; i++)
+			System.out.print(i); 
+
+	}
+}
