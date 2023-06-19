0469a35a15ce3755899d938e31763f0df7ae0db9
dayul <dyulchoo@naver.com>
2023-06-19 21:03:33 +0900

    for문, 1부터 10까지 수의 합 출력

---
+++ LoopPractice/src/forpractice/For1to10sum.java
@@ -0,0 +1,15 @@
+package forpractice;
+
+public class For1to10sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1; i<=10; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
