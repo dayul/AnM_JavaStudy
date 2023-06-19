05e8eac6e4647b538d1d5de384153fd7186a1b18
dayul <dyulchoo@naver.com>
2023-06-19 20:51:22 +0900

    두개의 문자열을 입력받고 순서 변경하여 출력

---
+++ CodingTest/src/suhang/Str_1118.java
@@ -0,0 +1,18 @@
+package suhang;
+import java.util.Scanner;
+public class Str_1118 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("문자를 입력하세요.: ");
+		a = sc.next();
+		
+		System.out.print("문자를 입력하세요.: ");
+		b = sc.next();
+		
+		System.out.println(b + " " + a);
+	}
+
+}
