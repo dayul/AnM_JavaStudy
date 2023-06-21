706cc5c96c7addd410ad2d84f91448adb29e355b
dayul <dyulchoo@naver.com>
2023-06-21 18:59:24 +0900

    정수 3개를 입력받아 합과 평균(소수 2자리까지)을 출력

---
+++ CodingTest/src/suhang/Integer_1118.java
@@ -0,0 +1,23 @@
+package suhang;
+import java.util.Scanner;
+public class Integer_1118 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a, b, c;
+		int sum = 0;
+		float average = 0;
+		System.out.print("정수 3개 입력: ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
+		sum = a + b + c;
+		average = (a + b + c) / 3;
+		System.out.println("합: " + sum);
+		System.out.println("평균 : " + String.format("%.2f", average));
+				//String.format() : 반올림 하는.. (안에 출력할 것)
+		
+	}
+
+}
