77c084918ded13303d5d68c31d7cabdd7918a74d
dayul <dyulchoo@naver.com>
2023-06-22 10:24:43 +0900

    빛이 하루 동안 가는 거리 출력

---
+++ CodingTest/src/suhang/Light_1118.java
@@ -0,0 +1,13 @@
+package suhang;
+
+public class Light_1118 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		long s = 86400;		// 24시간을 초로 환산
+		long light = 300000;
+		long day = s*light;
+		System.out.println("빛이 하루동안 가는 거리: " + day + " km");
+	}
+
+}
